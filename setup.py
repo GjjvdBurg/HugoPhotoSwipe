9b99d18e936a923c49272ddb9c76bba0b4460f1e
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2016-12-24 11:51:48 +0100

    fix classifiers

--- setup.py
+++ setup.py
@@ -19,13 +19,14 @@ setup(
         classifiers=[
             'Development Status :: 4 - Beta',
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-
-            # Python 2 and 3?
-            'Programming Language :: Python :: 3'
-            'Programming Language :: Python :: 3.0'
-            'Programming Language :: Python :: 3.1'
-            'Programming Language :: Python :: 3.2'
-            'Programming Language :: Python :: 3.3'
-            'Programming Language :: Python :: 3.4'
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             ],
         )
